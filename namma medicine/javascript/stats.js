165c419d1dc9475b4bd0c477d5671e2d33c45d57
RANJTH KUMAR <90784111+RANJITHKUMAR-E@users.noreply.github.com>
2023-02-23 19:53:22 +0530

    Events added

--- namma medicine/javascript/stats.js
+++ namma medicine/javascript/stats.js
@@ -1,18 +1,24 @@
 
 let valueDisplays = document.querySelectorAll(".num");
-let interval = 100;
+let interval=1000;
 
 
 valueDisplays.forEach((valueDisplay) => {
   let startValue = 0;
   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-  let duration = Math.floor(interval / endValue);
-  let counter = setInterval(function () {
-    startValue += 1;
-    valueDisplay.textContent = startValue;
-    if (startValue == endValue) {
+  let duration=Math.floor(interval/endValue);
+  let skip=0;
+  if(endValue<500) skip=1;
+  else skip=10;
+  let counter=setInterval(function()
+  {
+    if(parseInt(valueDisplay)<500) startValue+=1;
+    else startValue+=skip;
+    valueDisplay.textContent=startValue+"+";
+    if(startValue>=endValue) {
       clearInterval(counter);
     }
   }, duration);
+
 });
 
